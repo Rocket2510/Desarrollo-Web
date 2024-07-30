b37979bda9478038308fcbe65e1c1fdbf7356e1d
Rocket2510 <rocket251019@gmail.com>
2024-07-30 15:55:09 -0600

    Variables en JS

---
+++ IntroduccionJS/js/01-variables.js
@@ -0,0 +1,22 @@
+// variables
+let producto = 'Audifonos Gamer'; //iniciar variable y asignar valor
+
+let disponible; //iniciar variable pero sin valor
+
+producto = true; //reasignando el valor de la variable
+
+let producto1 = 'Computadora',
+    disponible1 = true,
+    categeria = 'Computadoras';
+
+/* Formas de crear variables */
+let nombre_producto = 'Monitor';//underscore
+let nombreProducto = 'Monitor HD' //camelCase
+let NombreProducto = 'Monitor HD' //PascalCase /* se usa para crear clases */
+
+
+
+
+
+
+
