d459e368c75a5a0dbbccfc4348eb5e3487cf0e5f
Rocket2510 <rocket251019@gmail.com>
2024-07-30 16:15:34 -0600

    Metodos para los Strings

--- IntroduccionJS/js/02-Tipos-de-datos.js
+++ IntroduccionJS/js/02-Tipos-de-datos.js
@@ -2,8 +2,25 @@
  * String o cadenas de texto
  */
 const producto = "Monitor de 20 pulgadas"
-const producto2 = String('Monitor 30 pulgadas');
-const producto3 = new String('Monitor 30 pulgadas');
+// const producto2 = String('Monitor 30 pulgadas');
+// const producto3 = new String('Monitor 30 pulgadas');
+
+/**
+ * Metodos para String
+ */
+const tweet = 'Aprendiendo JavaScript';
+const producto3 = 'Monitor LG';
+
+//.length para saber la extension en un String
+console.log(tweet.length);
+//.indexOf para encontrar un elemento en un String
+console.log(producto3.indexOf('LG'));
+console.log(tweet.indexOf('JavaScript'));
+//Includes (return boolean)
+console.log(producto3.includes('LG'));
+console.log(tweet.includes('JvaScript'));
+
+
 
 
 
