7bbcd495b3e7d131bacdb1f83ca24e2871ab29b0
Rocket2510 <rocket251019@gmail.com>
2024-07-30 16:24:49 -0600

    Números y Operadores

--- IntroduccionJS/js/02-Tipos-de-datos.js
+++ IntroduccionJS/js/02-Tipos-de-datos.js
@@ -6,7 +6,7 @@ const producto = "Monitor de 20 pulgadas"
 // const producto3 = new String('Monitor 30 pulgadas');
 
 /**
- * Metodos para String
+ * Metodos para Strings
  */
 const tweet = 'Aprendiendo JavaScript';
 const producto3 = 'Monitor LG';
@@ -20,7 +20,24 @@ console.log(tweet.indexOf('JavaScript'));
 console.log(producto3.includes('LG'));
 console.log(tweet.includes('JvaScript'));
 
+/**
+ * Numeros y operadores
+ */
+const num1 = 100;
+const num2 = 200;
+const num3 = 200.20;
+const num4 = .1020;
+const num5 = -5;
 
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
 
-
+console.log("suma:", num1 + num2);
+console.log("resta:", num2 - num1);
+console.log("mult:", num2 * num1);
+console.log("div:", num2 / num1);
+console.log("modulo:", num2 % num1);
 
