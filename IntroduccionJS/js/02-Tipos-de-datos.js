c9eda2b6217afcb8cee516a3e8ea47bbb77547a4
Rocket2510 <rocket251019@gmail.com>
2024-07-30 16:06:14 -0600

    Tipo String

---
+++ IntroduccionJS/js/02-Tipos-de-datos.js
@@ -0,0 +1,9 @@
+/**
+ * String o cadenas de texto
+ */
+const producto = "Monitor de 20 pulgadas"
+const producto2 = String('Monitor 30 pulgadas');
+const producto3 = new String('Monitor 30 pulgadas');
+
+
+
