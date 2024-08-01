65129c985652eb50bf4964397c808c1f544b2898
Rocket2510 <rocket251019@gmail.com>
2024-08-01 16:05:55 -0600

    Mas Metodos de Arrays

--- IntroduccionJS/js/08-arrays.js
+++ IntroduccionJS/js/08-arrays.js
@@ -30,14 +30,14 @@ const arreglo = ["Hola", 10, true, "si", null, { nombre: 'edgar', edad: 20 }, [1
  */
 
 //AGREGAR ELEMENTOS A UN ARREGLO
-numeros[5] = 60;
-console.table(numeros);
+// numeros[5] = 60;
+// console.table(numeros);
 
-numeros.push(70, 80, 90);//agrega al final del arreglo
-console.table(numeros);
+// numeros.push(70, 80, 90);//agrega al final del arreglo
+// console.table(numeros);
 
-numeros.unshift(-10, -20, - 30);//agrega al inicio del arreglo
-console.table(numeros);
+// numeros.unshift(-10, -20, - 30);//agrega al inicio del arreglo
+// console.table(numeros);
 
 //ELIMINAR ELEMENTOS DE UN ARREGLO
 //meses.pop(); //elimina el ultimo elemento de un arreglo
@@ -52,3 +52,47 @@ console.table(numeros);
 //const nuevoArreglo = ['Junio', ...meses]; //simula .unshift
 //console.log(nuevoArreglo);
 
+/**
+ * MAS METODOS PARA LOS ARRAYS
+ */
+
+const carrito = [
+    { nombre: 'Monitor 20 Pulgadas', precio: 500, },
+    { nombre: 'Television 50 Pulgadas', precio: 700, },
+    { nombre: 'Tablet', precio: 300, },
+    { nombre: 'Audifonos', precio: 200, },
+    { nombre: 'Teclado', precio: 50, },
+    { nombre: 'Celular', precio: 500, },
+    { nombre: 'Bocinas', precio: 300, },
+    { nombre: 'Laptop', precio: 800, },
+]
+
+//'forEach'
+meses.forEach(function (mes) {
+    if (mes == 'Marzo') {
+        console.log("Marzo si existe");
+    }
+});
+
+//'Includes'
+let resultado = meses.includes('Marzo'); // : true (Si existe Marzo)
+
+//'Some' ideal para arreglo de objetos
+resultado = carrito.some(function (producto) {
+    return producto.nombre === 'Celular' //true || false 
+});
+
+//'Reduce'
+resultado = carrito.reduce(function (total, producto) {
+    return total + producto.precio;  
+}, 0);
+
+//'Filter'
+resultado = carrito.filter(function (producto) {
+    return producto.precio > 400;
+});
+
+resultado = carrito.filter(function (producto) {
+    return producto.nombre === 'Celular';
+});
+console.log(resultado); // 
