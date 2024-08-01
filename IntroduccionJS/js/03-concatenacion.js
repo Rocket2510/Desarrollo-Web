bc8b8a8ced58bdff3ddfc2d88d31d0739aa9bad8
Rocket2510 <rocket251019@gmail.com>
2024-07-31 18:00:55 -0600

    Concatenación y Template Strings

---
+++ IntroduccionJS/js/03-concatenacion.js
@@ -0,0 +1,11 @@
+const nombre = 'Edgar';
+const email = 'correo@correo.com'
+
+//concatenacion
+console.log("Nombre Cliente: " + nombre + ' Email: ' + email);
+
+//Template Strings o Strings Literals
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`)
+
+
+
