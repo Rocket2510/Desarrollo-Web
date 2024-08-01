d196b8f0b3d6cb43bfd3ef6adc03a8e353115bd0
Rocket2510 <rocket251019@gmail.com>
2024-07-31 18:10:11 -0600

    Objetos

---
+++ IntroduccionJS/js/05-objetos.js
@@ -0,0 +1,14 @@
+/**
+ * Objetos
+ */
+
+const producto = {
+    nombreProducto: 'Monitor 20 Pulgadas',
+    precio: 300,
+    disponible: true,
+}
+
+console.log(producto);
+console.log(producto.precio);
+
+console.log(producto['precio']);
