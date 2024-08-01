2668c978829e184aeb1008686150b1b65942d6ba
Rocket2510 <rocket251019@gmail.com>
2024-07-31 19:41:38 -0600

    Metodos para los Arrays

--- IntroduccionJS/js/08-arrays.js
+++ IntroduccionJS/js/08-arrays.js
@@ -6,7 +6,7 @@ const numeros = [10, 20, 30, 40, 50];
 
 //console.table(numeros)
 
-const meses = ['Enero', 'Febrero', 'Marzo'];
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'];
 //console.log(meses);
 
 const arreglo = ["Hola", 10, true, "si", null, { nombre: 'edgar', edad: 20 }, [10, 20, "hola", true]];
@@ -20,6 +20,35 @@ const arreglo = ["Hola", 10, true, "si", null, { nombre: 'edgar', edad: 20 }, [1
 //console.log(meses.length);
 
 //RECORRER ARREGLOS
-numeros.forEach(function (numero) {
-    console.log(numero);
-});
+// numeros.forEach(function (numero) {
+//     console.log(numero);
+// });
+
+
+/**
+ * METODOS PARA LOS ARRAYS
+ */
+
+//AGREGAR ELEMENTOS A UN ARREGLO
+numeros[5] = 60;
+console.table(numeros);
+
+numeros.push(70, 80, 90);//agrega al final del arreglo
+console.table(numeros);
+
+numeros.unshift(-10, -20, - 30);//agrega al inicio del arreglo
+console.table(numeros);
+
+//ELIMINAR ELEMENTOS DE UN ARREGLO
+//meses.pop(); //elimina el ultimo elemento de un arreglo
+//meses.shift(); //elimina el primer elemento de un arreglo
+
+//meses.splice(2,1) //eliminar un elemento en especifico de un arreglo
+//console.table(meses); 
+
+/* No se debe modificar el array original, para eso se crea uno nuevo que si se pueda modificar*/
+// Rest Operator o Spread Operator
+//const nuevoArreglo = [...meses, 'Junio']; //simula .push
+//const nuevoArreglo = ['Junio', ...meses]; //simula .unshift
+//console.log(nuevoArreglo);
+
