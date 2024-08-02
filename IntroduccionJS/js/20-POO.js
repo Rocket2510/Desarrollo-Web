9a0816cf0b00cbf21f8bea7a0366fb5db5000cf9
Rocket2510 <rocket251019@gmail.com>
2024-08-02 17:47:12 -0600

    Herencia

--- IntroduccionJS/js/20-POO.js
+++ IntroduccionJS/js/20-POO.js
@@ -18,9 +18,19 @@ class Producto {
 }
 
 const producto2 = new Producto('Monitor Curvo de 49 Pulgadas', 2000, 12);
-const producto3 = new Producto('Laptop', 1000, 1);
-
 /**
  * HERENCIA
  */
 
+class Libro extends Producto {
+    constructor(nombre, precio, isbn) {
+        super(nombre, precio);
+        this.isbn = isbn;
+    }
+
+    formatearProducto() {
+        return `${super.formatearProducto()} y su ISBN es: ${this.isbn}`;
+    }
+}
+
+const libro = new Libro('JavaScript la Revolucion', 120, '23094823093847');
