e1d635e96e2932597a5995522ec4ea2cc9015937
Rocket2510 <rocket251019@gmail.com>
2024-08-02 17:18:50 -0600

    Prototypes

--- IntroduccionJS/js/19-object.js
+++ IntroduccionJS/js/19-object.js
@@ -10,6 +10,14 @@ const producto = {
 }
 
 /** Object Constructor */
+function Cliente(nombre, apellido) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+}
+
+Cliente.prototype.formatearCliente = function () {
+    return `El cliente: ${this.nombre} ${this.apellido}`;
+}
 function Producto(nombre, precio, stock) {
     this.nombre = nombre;
     this.precio = precio;
@@ -21,6 +29,23 @@ function Producto(nombre, precio, stock) {
     }
 }
 
+/**
+ * PROTOTYPES
+ * 
+ * Es una función que esta asociada a un tipo de Objeto
+ * Nos va a permitir crear funciones que solo se utilizan en un objeto en especifico
+ */
+
+Producto.prototype.formatearProducto = function () {
+    return `El producto ${this.nombre} tiene un precio de: $ ${this.precio}`;
+}
+
 const producto2 = new Producto('Monitor Curvo de 49 Pulgadas', 2000, 12);
 const producto3 = new Producto('Laptop', 1000, 1);
+const cliente = new Cliente('Edgar', 'Carrillo')
+
+console.log(producto3.formatearProducto());
+console.log(cliente.formatearCliente());
+
+
 
