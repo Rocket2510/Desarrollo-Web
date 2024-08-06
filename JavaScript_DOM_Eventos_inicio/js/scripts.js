71ab2d2236cd83ef24054382041a598885471263
Rocket2510 <rocket251019@gmail.com>
2024-08-06 13:13:48 -0600

    Seleccionar elementos con querySelector

---
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -0,0 +1,21 @@
+/**
+ * Seleccionar elementos con querySelector
+ * 
+ */
+
+/**
+ * querySelector
+ * Retona 0 o 1 Elementos
+ */
+const heading = document.querySelector('.header__texto h2');
+heading.textContent = 'Nuevo texto';
+
+
+/**
+ * querySelectorAll
+ */
+
+
+/**
+ * getElementById
+ */
