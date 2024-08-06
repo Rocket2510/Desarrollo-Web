fa74452aab723682688713890cd9d9c7caa3c827
Rocket2510 <rocket251019@gmail.com>
2024-08-06 14:55:53 -0600

    Eventos con el teclado

--- JavaScript_DOM_Eventos_inicio/js/scripts.js
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -89,3 +89,30 @@ btnEnviar.addEventListener('click', function (evento) {
     console.log('EVIANDO FORMULARIO...');
 })
 
+/**
+ * EVENTOS DE LOS INPUTS  Y TEXT AREA
+ */
+
+const datos = {
+    nombre: '',
+    email: '',
+    mensaje: '',
+}
+
+const nombre = document.querySelector('#nombre')
+const email = document.querySelector('#email')
+const mensaje = document.querySelector('#mensaje')
+
+nombre.addEventListener('input', leerTexto)
+
+email.addEventListener('input', leerTexto)
+
+mensaje.addEventListener('input', leerTexto)
+
+function leerTexto(e) {
+    console.log(e.target.value);
+
+    datos[e.target.id] = e.target.value;
+
+    console.log(datos);
+}
