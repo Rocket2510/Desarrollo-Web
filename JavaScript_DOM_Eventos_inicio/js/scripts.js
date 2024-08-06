e18d17c0b80fa32f1ea60df28a152178c890a412
Rocket2510 <rocket251019@gmail.com>
2024-08-06 14:10:06 -0600

    Eventos en JS

--- JavaScript_DOM_Eventos_inicio/js/scripts.js
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -49,3 +49,30 @@ nuevoEnlace.classList.add('navegacion__enlace');
 //5. Agregar al Documento HTML
 const navegacion = document.querySelector('.navegacion');
 navegacion.appendChild(nuevoEnlace);
+
+/**
+ * EVENTOS EN JAVASCRIPT
+ * 
+ */
+
+console.log(1);
+
+window.addEventListener('load', function () {
+    //'load' espera a que el JS y los archivos que dependen del HTML esten listos
+    console.log(2)
+});
+
+window.onload = function () {
+    console.log(3)
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    //'DOMContentLoaded' espera a que solo cargue el HTML, pero no espera CSS o imagenes
+    console.log(4);
+})
+
+console.log(5);
+
+window.onscroll = function(){
+    console.log('scrolling...');
+}
