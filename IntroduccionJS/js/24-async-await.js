27eceb242c8061d1e896cd75b1bd8e1fb48c234c
Rocket2510 <rocket251019@gmail.com>
2024-08-05 18:31:11 -0600

    trabajar con dos consultas async await

--- IntroduccionJS/js/24-async-await.js
+++ IntroduccionJS/js/24-async-await.js
@@ -32,7 +32,7 @@ async function app() {
     }
 }
 
-app();
+//app();
 
 //Se ejecuta una vez despues de 'x' tiempo
 // setTimeout(function () {
@@ -46,4 +46,38 @@ app();
 //     //descargarNuevosClientes();
 // }, 2000);
 
+/**
+ *  Como trabajar con dos consultas async await?
+ */
+
+function descargarUltimosPedidos() {
+    return new Promise(resolve => {
+        console.log(`DESCARGANDO PEDIDOS...ESPERE...`);
+
+        setTimeout(() => {
+            resolve(`Los pedidos fueron Descargados`);
+        }, 3000);
+    });
+}
+
+async function app2() {
+    try {
+        // console.log('--- trabajando con dos consultas async await... -- ')
+        // const clientes = await descargarNuevosClientes();
+        // const pedidos = await descargarUltimosPedidos();
+        // console.log(clientes);
+        // console.log(pedidos);
+
+
+        //"Promise.all()": Se ejecutan las 2 funciones al mismo tiempo
+        const resultado = await Promise.all([descargarNuevosClientes(), descargarUltimosPedidos()]);
+        console.log(resultado[0]);
+        console.log(resultado[1]);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+app2();
 
