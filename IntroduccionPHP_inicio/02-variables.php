a859db30058d1d082a52a15a29b247c40d82c957
Rocket2510 <rocket251019@gmail.com>
2024-08-16 17:46:06 -0600

    Variables y Constantes en PHP

--- IntroduccionPHP_inicio/02-variables.php
+++ IntroduccionPHP_inicio/02-variables.php
@@ -1,6 +1,16 @@
 <?php include 'includes/header.php';
+//variable que se puede reasignar
+$nombre = "Edgar\n";
 
+echo $nombre;
+var_dump($nombre);
 
+//variables constantes
+define('PI', 3.1416); //es mas comun esta sintaxis
+echo PI;
+
+const constante = 22;
+echo constante;
 
 
 include 'includes/footer.php';
