fc7abeb165f1783317f4b39e44b9373081276424
Rocket2510 <rocket251019@gmail.com>
2024-08-16 17:52:52 -0600

    Tipos de Dato en PHP

--- IntroduccionPHP_inicio/03-tipos-dato.php
+++ IntroduccionPHP_inicio/03-tipos-dato.php
@@ -1,6 +1,23 @@
 <?php include 'includes/header.php';
 
+//Boolean
+$logueado = true;
+var_dump($logueado);
 
+//Int
+$numero = 200;
+var_dump($numero);
 
+//Float
+$float = 20.3;
+var_dump($float);
+
+//Strings
+$nombre = "edgar";
+var_dump($nombre);
+
+//Arrays
+$array = [];
+var_dump($array);
 
 include 'includes/footer.php';
