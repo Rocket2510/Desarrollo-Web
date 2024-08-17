aedeb3a5aef33cba4b1a65eb70197ab301aa0075
Rocket2510 <rocket251019@gmail.com>
2024-08-16 18:01:00 -0600

    Números y Operadores en PHP

--- IntroduccionPHP_inicio/04-operadores.php
+++ IntroduccionPHP_inicio/04-operadores.php
@@ -1,6 +1,27 @@
 <?php include 'includes/header.php';
 
+$numero1 = 20;
+$numero2 = 10;
 
+//sumar
+echo "suma: ", $numero1 + $numero2;
+echo "<br>";
+
+//restar
+echo "resta: ", $numero1 - $numero2;
+echo "<br>";
+
+//multiplicacion
+echo "multipicacion: ", $numero1 * $numero2;
+echo "<br>";
+
+//Dividir
+echo "division: ", $numero1 / $numero2;
+echo "<br>";
+
+//multiplicar cierta cantidad de veces
+echo "cuadrado: ", 2 ** 3;
+echo "<br>";
 
 
 include 'includes/footer.php';
