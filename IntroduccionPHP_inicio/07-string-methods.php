a5bb099ce0b11fa7d4ef33e560d372520894bb42
Rocket2510 <rocket251019@gmail.com>
2024-08-16 19:45:07 -0600

    Funciones para Strings en PHP

--- IntroduccionPHP_inicio/07-string-methods.php
+++ IntroduccionPHP_inicio/07-string-methods.php
@@ -1,6 +1,35 @@
 <?php include 'includes/header.php';
 
+$nombreCliente = "Juan Pablo";
 
+//Conocer extension de un string
+echo strlen($nombreCliente);
+var_dump($nombreCliente);
 
+//Eliminar espacios en blanco
+$texto = trim($nombreCliente);
+echo strlen($texto);
+
+//convertirlo a mayusculas
+echo strtoupper($nombreCliente);
+
+//convertirlo a minusculas
+echo strtolower($nombreCliente);
+
+//validar strings
+$mail = "correo@correo.com";
+$mail2 = "Correo@correo.com";
+
+//remplazar algun lugar del string
+var_dump(strtolower($mail) == strtolower($mail2));
+
+echo str_replace('Juan', 'Javier', $nombreCliente);
+
+//Revisar si un String existe o no
+echo strpos($nombreCliente, 'Juan');
+
+$tipoCliente = "Premium";
+echo "<br>";
+echo "El cliente ".$tipoCliente." es ".$nombreCliente;
 
 include 'includes/footer.php';
