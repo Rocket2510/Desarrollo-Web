4bfa0cc90f8d776437d1e70970f4aba64934c901
Rocket2510 <rocket251019@gmail.com>
2024-08-16 20:05:06 -0600

    Arreglos y Funciones para Arreglos en PHP

--- IntroduccionPHP_inicio/08-arrays.php
+++ IntroduccionPHP_inicio/08-arrays.php
@@ -1,6 +1,30 @@
 <?php include 'includes/header.php';
 
+//ARREGLOS INDEXADOS
+$carrito = ["tablet", "television", "computadora"];
 
+echo "<pre>"; //util para ver el contenido de un array
+var_dump($carrito);
+echo "</pre>";
 
+echo $carrito[1];//acceder a un elemento del array
+
+//Añade elemento en el indice proporcionado
+$carrito[3] = "Nuevo producto...";
+
+//Añadir un elemento nuevo al final del array
+array_push($carrito, "Audifonos");
+
+//Añadir un elemento nuevo al principio del array
+array_unshift($carrito, "SmartWatch");
+
+echo "<pre>"; 
+var_dump($carrito);
+echo "</pre>";
+
+$clientes = array("Juan", "edgar", "lluvia");
+echo "<pre>"; 
+var_dump($clientes);
+echo "</pre>";
 
 include 'includes/footer.php';
