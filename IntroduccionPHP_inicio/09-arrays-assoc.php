8e365d33a8486b05555bd43ddd4a52c4c14748d5
Rocket2510 <rocket251019@gmail.com>
2024-08-16 20:32:06 -0600

    Associative Arrays en PHP

--- IntroduccionPHP_inicio/09-arrays-assoc.php
+++ IntroduccionPHP_inicio/09-arrays-assoc.php
@@ -1,6 +1,30 @@
 <?php include 'includes/header.php';
 
+//ARREGLOS ASOCIATIVOS
+$cliente = [
+    'nombre' => 'Edgar',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo'=> 'Premium',
+        'diponible'=> 100,
+    ]
+];
 
+//echo $cliente['informacion']['diponible'];
 
+/**
+ * AGREGAR MAS ELEMENTOS AL ARREGLO
+ */
+
+ $cliente['codigo'] = 1231423223; 
+
+ $cliente['informacion']['sucursal'] = 'Polanco';
+ $cliente['informacion']['edad'] = 24;
+
+ echo "<pre>"; 
+ var_dump($cliente);
+ echo "</pre>";
+ 
+ echo $cliente['codigo'];
 
 include 'includes/footer.php';
