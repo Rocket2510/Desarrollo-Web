3d64071f2fa0ce36c7c5232a879f97481ef044c5
Rocket2510 <rocket251019@gmail.com>
2024-09-03 15:20:57 -0600

    Revisar si un elemento existe en un array y otros métodos

--- IntroduccionPHP_inicio/11-funciones-arrays.php
+++ IntroduccionPHP_inicio/11-funciones-arrays.php
@@ -1,6 +1,69 @@
 <?php include 'includes/header.php';
+/**
+ * `in_array()`:
+ * busca elementos en un arreglo
+ */
+ 
+$carrito = ['Tablet', 'Computadora', 'Television'];
 
+var_dump(in_array('Tablet', $carrito));
+var_dump(in_array('audifonos', $carrito));
 
+/**
+ * `sort()`:
+ * Ordenar elementos de un arreglo de menor a mayor
+ * 
+ * `rsort()`:
+ * Ordenar elementos de un arreglo de mayor a menor
+ * 
+ */
+$numeros = [1, 3, 4, 5, 1, 2];
+//rsort($numeros);
+
+//echo "<pre>";
+//var_dump($numeros);
+//echo "</pre>";
+
+/**
+ * Ordenar arreglo asociativo
+ */
+$cliente = [
+    'saldo' => 200,
+    'tipo'=> 'Premium',
+    'nombre'=> 'Juan'
+];
+
+/**
+ * `asort()`
+ * 
+ * Ordena por valores (orden alfabetico)
+ */
+//asort( $cliente);
+
+/**
+ * `ksort()`
+ * 
+ * Ordena por llaves (orden alfabetico)
+ */
+//ksort( $cliente);
+
+/**
+ * `krsort()`
+ * 
+ * Ordena por llaves (orden alfabetico, DE LA `Z` A LA `A`)
+ */
+//krsort( $cliente);
+
+/**
+ * `arsort()`
+ * 
+ * Ordena por valores (Z primero)
+ */
+arsort( $cliente);
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
 
 
 
