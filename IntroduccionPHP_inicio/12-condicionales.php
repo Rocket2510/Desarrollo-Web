a068a76b4d0b28b6bfc816ee80add960541fcb9b
Rocket2510 <rocket251019@gmail.com>
2024-09-03 15:47:54 -0600

    Condicionales el PHP

--- IntroduccionPHP_inicio/12-condicionales.php
+++ IntroduccionPHP_inicio/12-condicionales.php
@@ -1,7 +1,70 @@
 <?php include 'includes/header.php';
 
 
+$autenticado = false;
+$admin = false;
 
+/**
+ * if()
+ */
 
+if($autenticado && $admin) {
+    echo 'Usuario autenticado Correctamente';
+}else{
+    echo 'No tienes acceso';
+}
+
+/**
+ * if() anidados
+ */
+$cliente = [
+    'nombre'=> 'Edgar',
+    'saldo'=> 200,
+    'informacion'=> [
+        'tipo'=> 'Premium',
+    ]
+];
+echo '<br>';
+if(!empty($cliente)){
+    echo 'El arreglo de cliente no esta vacio';
+
+    if($cliente['saldo'] > 0){
+        echo 'El cliente tiene saldo disponible: $' , $cliente['saldo'];
+    }else{
+        echo 'no hay saldo' ;
+    }
+}
+
+/**
+ * else if()
+ */
+
+echo '<br>';
+if($cliente['saldo'] > 0){
+    echo 'el cliente tiene saldo';
+}else if($cliente['informacion']['tipo'] === 'Premium'){
+    echo 'el cliente es premium';
+}else{
+    echo 'no hay cliente definido';
+}
+
+/**
+ * switch()
+ */
+echo '<br>';
+$tecnologia = 'Java';
+
+ switch($tecnologia){
+    case 'PHP': 
+        echo 'PHP, un excelente lenguaje';
+        break;
+    case 'Java':
+        echo 'Java, es un buen lenguaje';
+        break;
+
+    default;
+        echo 'Ese lenguaje no lo conozco';
+        break;
+ }
 
 include 'includes/footer.php';
