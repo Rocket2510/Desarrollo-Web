bec4f755cae156d2fd58bb2e7c3dfb2b180a9d78
Rocket2510 <rocket251019@gmail.com>
2024-09-03 17:09:22 -0600

    loops en php

--- IntroduccionPHP_inicio/13-loops.php
+++ IntroduccionPHP_inicio/13-loops.php
@@ -1,6 +1,42 @@
 <?php include 'includes/header.php';
 
+/**
+ * while
+ */
 
+$i = 1; //incremento
+while($i < 10){
+    echo $i.'<br>';
 
+    $i++;
+}
+
+echo'<br>';
+/**
+ * Do-while
+ */
+$i = 11;
+do{
+    echo $i.'<br>';
+    $i++;
+}while($i < 10);
+echo'<br>';
+
+/**
+ * For Loop
+ */
+
+for($i = 0; $i <= 10; $i++){
+    if($i % 3 === 0 && $i % 5 === 0){
+        echo $i.': es fizzbuzz';
+        echo'<br>';
+    }else if($i % 3 === 0){
+        echo $i.': es fizz';
+        echo'<br>';
+    }elseif($i % 5 === 0){
+        echo $i.': es buzz';
+        echo'<br>';
+    }
+}
 
 include 'includes/footer.php';
