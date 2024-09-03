035a6c3a2b6da6edb3126078409e9b2ac1c11a6e
Rocket2510 <rocket251019@gmail.com>
2024-09-03 17:33:49 -0600

    foreach

--- IntroduccionPHP_inicio/13-loops.php
+++ IntroduccionPHP_inicio/13-loops.php
@@ -4,39 +4,64 @@
  * while
  */
 
-$i = 1; //incremento
-while($i < 10){
-    echo $i.'<br>';
+// $i = 1; //incremento
+// while($i < 10){
+//     echo $i.'<br>';
 
-    $i++;
-}
+//     $i++;
+// }
 
-echo'<br>';
+// echo'<br>';
 /**
  * Do-while
  */
-$i = 11;
-do{
-    echo $i.'<br>';
-    $i++;
-}while($i < 10);
-echo'<br>';
+// $i = 11;
+// do{
+//     echo $i.'<br>';
+//     $i++;
+// }while($i < 10);
+// echo'<br>';
 
 /**
  * For Loop
  */
 
-for($i = 0; $i <= 10; $i++){
-    if($i % 3 === 0 && $i % 5 === 0){
-        echo $i.': es fizzbuzz';
-        echo'<br>';
-    }else if($i % 3 === 0){
-        echo $i.': es fizz';
-        echo'<br>';
-    }elseif($i % 5 === 0){
-        echo $i.': es buzz';
-        echo'<br>';
-    }
+// for($i = 0; $i <= 10; $i++){
+//     if($i % 3 === 0 && $i % 5 === 0){
+//         echo $i.': es fizzbuzz';
+//         echo'<br>';
+//     }else if($i % 3 === 0){
+//         echo $i.': es fizz';
+//         echo'<br>';
+//     }elseif($i % 5 === 0){
+//         echo $i.': es buzz';
+//         echo'<br>';
+//     }
+// }
+
+/**
+ * for Each
+ */
+
+$clientes = ['Pedro', 'Juan', 'Edgar'];
+
+foreach($clientes as $cliente){
+    echo $cliente. '<br>';
+}
+echo '<br>';
+// for($i=0; $i < count($clientes); $i++){
+//     echo $clientes[$i]. '<br>';
+
+// }
+
+$cliente = [
+    'nombre' => 'EDGAR',
+    'saldo' => 200,
+    'tipo' => 'Premium'
+];
+
+foreach($cliente as $key => $c){
+    echo $key. "-".$c. '<br>';
 }
 
 include 'includes/footer.php';
