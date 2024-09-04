441dec5c78e782a84d3d78ccc1b8bdd3675d47a0
Rocket2510 <rocket251019@gmail.com>
2024-09-03 21:36:26 -0600

    require2

--- IntroduccionPHP_inicio/17-require.php
+++ IntroduccionPHP_inicio/17-require.php
@@ -1,6 +1,18 @@
-<?php include 'includes/header.php';
+<?php 
+/**
+ * `include`:
+ * 
+ * Incluye otros archivos para reutilizar y evitar repeticion de código
+ * => para añadir otros templates 
+ * 
+ * `require`:
+ * => usar cuando tengamos funciones: Ej: Conexion a una DB
+ * 
+ */
+include 'includes/header.php'; 
 
-
+require 'funciones.php';
+iniciarApp();
 
 
 
