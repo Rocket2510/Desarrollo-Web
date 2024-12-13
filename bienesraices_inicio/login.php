f1a5663c8ea3bed3ba706b143c0b8650d1957aae
Rocket2510 <rocket251019@gmail.com>
2024-12-12 19:08:49 -0600

    Validaciones al Formulario

--- bienesraices_inicio/login.php
+++ bienesraices_inicio/login.php
@@ -1,4 +1,36 @@
 <?php
+
+    //conexion a la BD
+    require 'includes/config/database.php';
+    $db = conectarDB();
+
+
+    //Autenticar el usuario
+    $errores = [];
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        echo "<pre>";
+        var_dump($_POST);
+        echo "</pre>";
+
+        $email = mysqli_real_escape_string($db,filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) ;
+        $password =mysqli_real_escape_string($db,$_POST['password']);
+
+        if (!$email) {
+            $errores[] = "El email, es obligatorio o no es valido";
+        }
+
+        if (!$password) {
+            $errores[] = "El password es obligatorio";
+        }
+
+        if(empty($errores)){
+            $errores [] = ""
+        }
+    }
+
+
+    //Incluye el Header
     require 'includes/funciones.php';
     incluirTemplate('header');
     
@@ -7,15 +39,22 @@
     <main class="contenedor seccion contenido-centrado">
         <h1>Iniciar Sesión</h1>
 
-        <form  class="formulario" action="">
+        <?php  foreach($errores as $error): ?>
+            <div class="alerta error">
+                <?php echo $error; ?>
+            </div>
+                
+        <?php endforeach; ?>
+
+        <form method="POST" action="" class="formulario">
             <fieldset>
                     <legend>Email y Password</legend>
 
                     <label for="email">E-mail</label>
-                    <input type="email" placeholder="Tu E-mail" id="email">
+                    <input type="email" name="email" placeholder="Tu E-mail" id="email" required>
 
                     <label for="password">Password</label>
-                    <input type="password" placeholder="Tu Password" id="pasword">
+                    <input type="password" name="password" placeholder="Tu Password" id="pasword" required>
                 </fieldset>
 
                 <input type="submit" value="Iniciar Sesion" class="boton boton-verde">
