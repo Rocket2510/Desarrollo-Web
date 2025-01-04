86d78538697751176453068b6438032e1fb1a94e
Rocket2510 <rocket251019@gmail.com>
2025-01-04 16:32:02 -0600

    Creando una Clase e instanciandola

--- IntroduccionPHPOOP_Inicio/01.php
+++ IntroduccionPHPOOP_Inicio/01.php
@@ -1,4 +1,34 @@
 <?php include 'includes/header.php';
+//Definir clase
+class Producto{
+    //Agregar atributos
+    public $nombre;
+    public $precio;
+    public $disponible;
+
+}
+
+//Instanciar la clase
+$producto = new Producto();
+
+$producto -> nombre = 'Tablet'; 
+$producto -> precio = 200; 
+$producto -> disponible = true; 
+
+echo "<pre>";
+var_dump($producto);
+echo "</pre>";
+
+$producto2 = new Producto();
+$producto2 -> nombre = 'Monitor Curvo';
+$producto2 -> precio = 300;
+$producto2 -> disponible = true;
+
+echo "<pre>";
+var_dump($producto2);
+echo "</pre>";
+
 
 
 include 'includes/footer.php';
+
