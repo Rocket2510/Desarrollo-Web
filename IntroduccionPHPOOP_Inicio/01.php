03f685fb561fd9f606b7a7a6ddd537f3a589c247
Rocket2510 <rocket251019@gmail.com>
2025-01-04 16:40:36 -0600

    Atributos de la clase y Constructores

--- IntroduccionPHPOOP_Inicio/01.php
+++ IntroduccionPHPOOP_Inicio/01.php
@@ -1,4 +1,6 @@
-<?php include 'includes/header.php';
+<?php 
+declare (strict_types = 1);
+include 'includes/header.php';
 //Definir clase
 class Producto{
     //Agregar atributos
@@ -6,29 +8,36 @@ class Producto{
     public $precio;
     public $disponible;
 
+    /**
+     * El constructor es una funcion que se va a mandar llamar automaticamente
+     * una vez que se haya creado una nueva instancia 
+     */
+     public function __construct(string $nombre, int $precio, bool $disponible){
+        $this -> nombre = $nombre;
+        $this -> precio = $precio;
+        $this -> disponible = $disponible;
+     }
 }
 
 //Instanciar la clase
-$producto = new Producto();
+$producto = new Producto('Tablet', 200, true);
 
-$producto -> nombre = 'Tablet'; 
-$producto -> precio = 200; 
-$producto -> disponible = true; 
+// $producto -> nombre = 'Tablet'; 
+// $producto -> precio = 200; 
+// $producto -> disponible = true; 
 
 echo "<pre>";
 var_dump($producto);
 echo "</pre>";
 
-$producto2 = new Producto();
-$producto2 -> nombre = 'Monitor Curvo';
-$producto2 -> precio = 300;
-$producto2 -> disponible = true;
+$producto2 = new Producto('Monitor Curvo', 300, true);
+// $producto2 -> nombre = 'Monitor Curvo';
+// $producto2 -> precio = 300;
+// $producto2 -> disponible = true;
 
 echo "<pre>";
 var_dump($producto2);
 echo "</pre>";
-
-
 
 include 'includes/footer.php';
 
