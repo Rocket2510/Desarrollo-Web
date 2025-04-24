8c93947703ba4286672f46502b45b2839c3dec12
Rocket2510 <rocket251019@gmail.com>
2025-04-23 21:25:17 -0600

    Métodos en classes

--- IntroduccionPHPOOP_Inicio/01.php
+++ IntroduccionPHPOOP_Inicio/01.php
@@ -4,23 +4,39 @@ include 'includes/header.php';
 //Definir clase
 class Producto{
     //Agregar atributos
-    public $nombre;
-    public $precio;
-    public $disponible;
+     public $nombre; // EN PHP 8 ya no es necesario declarar asi los atributos 
+     public $precio;
+     public $disponible;
 
     /**
      * El constructor es una funcion que se va a mandar llamar automaticamente
      * una vez que se haya creado una nueva instancia 
      */
      public function __construct(string $nombre, int $precio, bool $disponible){
-        $this -> nombre = $nombre;
-        $this -> precio = $precio;
-        $this -> disponible = $disponible;
+        /**
+         * En PHP 8 ya no es necesario declarar asi los atributos del constructor
+         */
+         $this -> nombre = $nombre;
+         $this -> precio = $precio;
+         $this -> disponible = $disponible;
+
+        
+     }
+
+     /**
+      * En el mismo constructor se pueden declarar los atributos en PHP 8
+      */
+     //public function __construct(public string $nombre,public int $precio,public bool $disponible){}
+
+     /** METODOS EN CLASES */
+     public function mostrarProducto(){
+        echo "El Producto es: " . $this->nombre . " y su Precio es: " . $this->precio;
      }
 }
 
 //Instanciar la clase
 $producto = new Producto('Tablet', 200, true);
+$producto->mostrarProducto();
 
 // $producto -> nombre = 'Tablet'; 
 // $producto -> precio = 200; 
@@ -31,6 +47,7 @@ var_dump($producto);
 echo "</pre>";
 
 $producto2 = new Producto('Monitor Curvo', 300, true);
+$producto2->mostrarProducto();
 // $producto2 -> nombre = 'Monitor Curvo';
 // $producto2 -> precio = 300;
 // $producto2 -> disponible = true;
