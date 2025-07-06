9ff408aedf6dd8db30bca907a3c196a555156583
Rocket2510 <rocket251019@gmail.com>
2025-07-06 17:34:48 -0600

    Autoload de Classes

--- IntroduccionPHPOOP_Inicio/08.php
+++ IntroduccionPHPOOP_Inicio/08.php
@@ -1,4 +1,22 @@
 <?php include 'includes/header.php';
+//Incluir las otras clases
+//require 'clases/Clientes.php';
+//require 'clases/Detalles.php';
+
+//AUTO LOAD de clases
+/**
+ * un metodo en PHP para automatizar la importaciones de clases 
+ * 
+ */
+function mi_autoload($clase){
+    require __DIR__ . '/clases/' . $clase . ".php";
+}
+
+spl_autoload_register('mi_autoload');
+
+$detalles = new Detalles();
+$clientes = new Clientes();
+
 
 
 include 'includes/footer.php';
