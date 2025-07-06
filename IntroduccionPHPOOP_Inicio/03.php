47e2e5fa423807276d75e14b83c8b9343319decb
Rocket2510 <rocket251019@gmail.com>
2025-07-06 00:31:37 -0600

    Metodos Static

--- IntroduccionPHPOOP_Inicio/03.php
+++ IntroduccionPHPOOP_Inicio/03.php
@@ -1,4 +1,93 @@
-<?php include 'includes/header.php';
+<?php 
+declare (strict_types = 1);
+include 'includes/header.php';
 
+//METODOS ESTATICOS
+
+/**
+ * No requieren Instaciarse 
+ */
+class Producto{
+    //Atributos Static
+    public $imagen;
+    static $imagenPlaceholder = "Imagen.jpg";
+
+    //Agregar atributos
+     protected $nombre; // EN PHP 8 ya no es necesario declarar asi los atributos 
+     public $precio;
+     public $disponible;
+
+    /**
+     * El constructor es una funcion que se va a mandar llamar automaticamente
+     * una vez que se haya creado una nueva instancia 
+     */
+     public function __construct(string $nombre, int $precio, bool $disponible, string $imagen){
+        /**
+         * En PHP 8 ya no es necesario declarar asi los atributos del constructor
+         */
+         $this -> nombre = $nombre;
+         $this -> precio = $precio;
+         $this -> disponible = $disponible;
+
+         if($imagen){
+            self::$imagenPlaceholder = $imagen;
+         }
+
+        
+     }
+
+     /**
+      * En el mismo constructor se pueden declarar los atributos en PHP 8
+      */
+     //public function __construct(public string $nombre,public int $precio,public bool $disponible){}
+
+     /** METODOS EN CLASES */
+     public static function obtenerImagenProducto(){
+        return self::$imagenPlaceholder;
+     }
+
+     public function mostrarProducto():void{
+        echo "El Producto es: " . $this->nombre . " y su Precio es: " . $this->precio;
+     }
+
+     public function getNombre():string{ 
+        return $this->nombre;
+    }
+
+     public function setNombre(string $nombre){
+        $this->nombre = $nombre;
+    }
+}
+
+//echo Producto::obtenerImagenProducto();
+
+//Instanciar la clase
+$producto = new Producto('Tablet', 200, true, '');
+//$producto->mostrarProducto();
+
+echo $producto->obtenerImagenProducto();
+
+echo $producto->getNombre();
+//$producto->setNombre("Nuevo nombre");
+
+// $producto -> nombre = 'Tablet'; 
+// $producto -> precio = 200; 
+// $producto -> disponible = true; 
+
+echo "<pre>";
+var_dump($producto);
+echo "</pre>";
+
+$producto2 = new Producto('Monitor Curvo', 300, true, "monitor.jpg");
+echo $producto->obtenerImagenProducto();
+//$producto2->mostrarProducto();
+// $producto2 -> nombre = 'Monitor Curvo';
+// $producto2 -> precio = 300;
+// $producto2 -> disponible = true;
+
+echo "<pre>";
+var_dump($producto2);
+echo "</pre>";
 
 include 'includes/footer.php';
+
